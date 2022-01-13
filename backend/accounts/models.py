d653f790301d30b9a14711a6fa6b211c26bb3cc2
jundm <bnmva23@gmail.com>
2022-01-14 02:32:43 +0900

    test:깃허브 테스츠중

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -55,3 +55,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
             return self.avatar.url
         else:
             return resolve_url("pydenticon_image", self.username)
+
+
+# 깃허브 테스트중입니다
