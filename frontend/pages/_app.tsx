ac293f50320ada3975b8e16f544f4b83a4f0b7f0
jundm <bnmva23@gmail.com>
2022-01-23 08:36:37 +0900

    front:setting redux persist

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,17 +1,22 @@
-import "@/styles/global.css";
 import type { AppProps } from "next/app";
-import HeaderBig from "@/components/HeaderBig";
 import { useState } from "react";
+import HeaderBig from "@/components/HeaderBig";
 import HeaderSmall from "@/components/HeaderSmall";
+import { wrapper } from "../utils/Toolkit/store";
+import "@/styles/global.css";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       {isOpen ? (
         <HeaderSmall
-        // saveLocalStorage={saveLocalStorage}
-        setIsOpen={setIsOpen}
+          // saveLocalStorage={saveLocalStorage}
+          setIsOpen={setIsOpen}
         />
       ) : (
         <HeaderBig
@@ -23,3 +28,5 @@ export default function App({ Component, pageProps }: AppProps) {
     </>
   );
 }
+
+export default wrapper.withRedux(App);
