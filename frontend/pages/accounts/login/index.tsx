aa8be46acb4e2488acf49bb307d0598813d4743b
jundm <bnmva23@gmail.com>
2022-01-29 10:34:10 +0900

    [FE] async-validator warning remove

--- frontend/pages/accounts/login/index.tsx
+++ frontend/pages/accounts/login/index.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Button, Card, Checkbox, Form, Input } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import axios from "axios";
 import { setLoginToken } from "@utils/Cookies/TokenManager";
+import Link from "next/link";
 
 interface LoginProps {}
 
+//TODO Remember me 적용
 function Login({}: LoginProps) {
+  const loginWidth = 300;
   const [form] = Form.useForm();
   const [isLoading, setIsLoading] = useState(false);
   const [inputs, setInputs] = useState({
@@ -39,7 +42,6 @@ function Login({}: LoginProps) {
       .then((res) => {
         const accessToken = res.data.access;
         const refreshToken = res.data.refresh;
-        console.log(res.data);
         setLoginToken(accessToken, refreshToken);
       })
       .catch((e) => console.warn(e.message));
@@ -48,70 +50,82 @@ function Login({}: LoginProps) {
   };
 
   return (
-    <Form
-      name="normal_login"
-      className="login-form w-96"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      style={{ margin: "auto" }}
-      form={form}
+    <Card
+      title="Login"
+      bordered={true}
+      style={{ width: `${loginWidth + 50}px`, margin: "0 auto" }}
     >
-      <Form.Item
-        name="email"
-        rules={[
-          {
-            type: "email",
-            message: "이메일 형식으로 입력하세요!",
-          },
-          {
-            required: true,
-            message: "이메일을 입력하세요!",
-          },
-        ]}
+      <Form
+        name="normal_login"
+        className="login-form w-96"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        style={{ margin: "auto" }}
+        form={form}
       >
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          placeholder="이메일"
+        <Form.Item
           name="email"
-          onChange={onChange}
-          value={email}
-        />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: "Please input your Password!" }]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="비밀번호"
+          rules={[
+            {
+              type: "email",
+              message: "이메일 형식으로 입력하세요!",
+            },
+            {
+              required: true,
+              message: "이메일을 입력하세요!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="이메일"
+            name="email"
+            onChange={onChange}
+            value={email}
+            style={{ width: loginWidth }}
+          />
+        </Form.Item>
+        <Form.Item
           name="password"
-          onChange={onChange}
-          value={password}
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="비밀번호"
+            name="password"
+            onChange={onChange}
+            value={password}
+            style={{ width: loginWidth }}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Link href="">
+            <a className="login-form-forgot">비밀번호를 잊으셨나요?</a>
+          </Link>
         </Form.Item>
 
-        <a className="login-form-forgot" href="">
-          비밀번호를 잊으셨나요?
-        </a>
-      </Form.Item>
-
-      <Form.Item>
-        <Button
-          type="primary"
-          htmlType="submit"
-          className="login-form-button"
-          disabled={isLoading}
-        >
-          Log in
-        </Button>
-        Or <a href="">지금 회원가입하기!</a>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            disabled={isLoading}
+            style={{ width: "78%" }}
+          >
+            Log in
+          </Button>
+          <br />
+          <Link href="/accounts/signup">
+            <a>지금 회원가입하기!</a>
+          </Link>
+        </Form.Item>
+      </Form>
+    </Card>
   );
 }
 
