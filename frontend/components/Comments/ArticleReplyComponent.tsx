062b11b930a7095a6d9be55861d978550a048fb8
jundm <bnmva23@gmail.com>
2022-03-21 19:05:29 +0900

    feat:add reply like mutate

--- frontend/components/Comments/ArticleReplyComponent.tsx
+++ frontend/components/Comments/ArticleReplyComponent.tsx
@@ -1,15 +1,33 @@
 import { Avatar, Comment } from "antd";
 import dayjs from "dayjs";
-import { useRouter } from "next/router";
 import { LikeOutlined, LikeFilled, EditOutlined } from "@ant-design/icons";
 import React, { useEffect } from "react";
 import useFetch from "@utils/Hook/useFetch";
 import { setVerifyToken } from "@utils/Cookies/TokenManager";
+import axios from "axios";
 
 interface ArticleReplyComponentProps {
   id: number;
 }
-
+interface CommentProps {
+  id: number;
+  author: {
+    username: string;
+    name: string;
+    avatar_url: string;
+  };
+  likes: number;
+  content: string;
+  isLikes: false;
+  created_at: string;
+  updated_at: string;
+}
+interface DataCommentProps {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: [CommentProps];
+}
 function ArticleReplyComponent({ id }: ArticleReplyComponentProps) {
   const { data, error, mutate } = useFetch(`posts/api/${id}/commentsReply/`);
   useEffect(() => {
@@ -20,16 +38,71 @@ function ArticleReplyComponent({ id }: ArticleReplyComponentProps) {
   }, [error, data]);
   return (
     <>
-      {data?.results?.map((reply: any, key: number) => {
+      {data?.results?.map((reply: CommentProps, key: number) => {
         return (
           <Comment
             key={key}
             author={<a>{reply.author.username}</a>}
             actions={[
               <>
-                <div className="flex items-center">
-                  <LikeOutlined />
-                  <span className="ml-1 cursor-pointer">like</span>
+                <div className="flex">
+                  {reply.isLikes ? (
+                    <div
+                      className="cursor-pointer flex items-center"
+                      onClick={async () => {
+                        mutate((likes: DataCommentProps) => {
+                          return {
+                            ...likes,
+                            results: [
+                              ...likes.results.map((result: CommentProps) =>
+                                result.id === reply.id
+                                  ? {
+                                      ...result,
+                                      isLikes: false,
+                                      likes: result.likes - 1,
+                                    }
+                                  : result
+                              ),
+                            ],
+                          };
+                        }, false);
+                        await axios.delete(
+                          `${process.env.NEXT_PUBLIC_ENV_BASE_URL}posts/api/${id}/commentsReply/${reply.id}/like/`
+                        );
+                      }}
+                    >
+                      <LikeFilled className="mr-1" />
+                      like
+                    </div>
+                  ) : (
+                    <div
+                      className="cursor-pointer flex items-center"
+                      onClick={async () => {
+                        mutate((likes: DataCommentProps) => {
+                          return {
+                            ...likes,
+                            results: [
+                              ...likes.results.map((result: CommentProps) =>
+                                result.id === reply.id
+                                  ? {
+                                      ...result,
+                                      isLikes: true,
+                                      likes: result.likes + 1,
+                                    }
+                                  : result
+                              ),
+                            ],
+                          };
+                        }, false);
+                        await axios.post(
+                          `${process.env.NEXT_PUBLIC_ENV_BASE_URL}posts/api/${id}/commentsReply/${reply.id}/like/`
+                        );
+                      }}
+                    >
+                      <LikeOutlined className="mr-1" />
+                      like
+                    </div>
+                  )}
                 </div>
               </>,
             ]}
