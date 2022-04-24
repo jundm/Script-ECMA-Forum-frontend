7721b4a277bef916fd658331176eea0d228035fd
jundm <bnmva23@gmail.com>
2022-04-24 10:39:19 +0900

    fix:set a proxy api

--- frontend/pages/api/proxy/[...slug].ts
+++ frontend/pages/api/proxy/[...slug].ts
@@ -1,21 +1,26 @@
-import { Request, ParamsDictionary, Response } from 'express-serve-static-core';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import { ParsedQs } from 'qs';
+import { Request, ParamsDictionary, Response } from "express-serve-static-core";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { ParsedQs } from "qs";
 
 // Create proxy instance outside of request handler function to avoid unnecessary re-creation
 const apiProxy = createProxyMiddleware({
-    target: 'http://localhost:5000',
-    changeOrigin: true,
-    pathRewrite: { [`^/api/proxy`]: '' },
-    secure: false,
+  target: "https://backend.sef.today",
+  changeOrigin: true,
+  pathRewrite: { [`^/api/proxy`]: "" },
+  secure: false,
 });
 
-export default function (req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>, number>) {
-    apiProxy(req, res, (result) => {
-        if (result instanceof Error) {
-            throw result;
-        }
+export default function (
+  req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+  res: Response<any, Record<string, any>, number>
+) {
+  apiProxy(req, res, (result) => {
+    if (result instanceof Error) {
+      throw result;
+    }
 
-        throw new Error(`Request '${req.url}' is not proxied! We should never reach here!`);
-    });
-};
+    throw new Error(
+      `Request '${req.url}' is not proxied! We should never reach here!`
+    );
+  });
+}
